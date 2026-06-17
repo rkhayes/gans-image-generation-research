ed8b495a45079735dea6a05fe2555d78947955fe
hayes <diogobonofre@gmail.com>
2026-06-17 06:58:02 -0300

    Adapted model source code to run on cuda Also added images from the second model evaluation now with 50 epochs

--- main.py
+++ main.py
@@ -1,6 +1,128 @@
-def main():
-    print("Hello from gans-image-generation-research!")
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.utils as utils
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Running on: {device}")
 
-if __name__ == "__main__":
-    main()
+x = torchvision.datasets.MNIST(
+    root='./data',
+    train=True,
+    download=True,
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=0.5, std=0.5)
+    ])
+)
+
+x_data_loader = torch.utils.data.DataLoader(
+    x,
+    batch_size=128,
+    shuffle=True
+)
+
+# Por que 0.2 de inclinação? (Plot do gráfico gerado talvez ajude)
+# Onde entra o input x? (Necessário programar forward pass)
+class D(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.nn = nn.Sequential(
+            nn.Linear(784, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        x_flat = x.view(batch_size, 784)
+        return self.nn(x_flat)
+
+class G(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.nn = nn.Sequential(
+            nn.Linear(100, 256),
+            nn.ReLU(),
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 784),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        x_g_flat = self.nn(z)
+        # Usar -1 no .view() do PyTorch faz com que ele calcule o tamanho do
+        # batch automaticamente com base no que sobrou dos dados.
+        # Evita problemas caso o último lote seja menor do que o normal.
+        return x_g_flat.view(-1, 1, 28, 28)
+
+d = D().to(device)
+g = G().to(device)
+
+# Por que BCE Loss? (Necessita pesquisa mais profunda)
+loss_function = nn.BCELoss()
+
+# Por que os otimizadores isolados?
+optimizer_d = optim.Adam(d.parameters(), lr=0.0002)
+optimizer_g = optim.Adam(g.parameters(), lr=0.0002)
+
+epochs = 50
+fixed_noise = torch.randn((9, 100)).to(device)
+
+for epoch in range(epochs):
+    # Discard original labels from MNIST with _
+    for x_batch, _ in x_data_loader:
+        x_batch_size = x_batch.size(0)
+
+        # Move the batch to GPU
+        x_batch = x_batch.to(device)
+
+        x_labels = torch.ones((x_batch_size, 1)).to(device)
+        z_labels = torch.zeros((x_batch_size, 1)).to(device)
+        z = torch.randn((x_batch_size, 100)).to(device)
+
+        # Training the discriminator
+        d.train()
+        optimizer_d.zero_grad()
+
+        # Passing real images to D
+        x_predictions = d(x_batch)
+        x_loss = loss_function(x_predictions, x_labels)
+
+        # Passing fake images to D (isolating the G gradient)
+        z_predictions = d(g(z).detach())
+        z_loss = loss_function(z_predictions, z_labels)
+
+        # Updating D weights
+        total_loss = x_loss + z_loss
+        total_loss.backward()
+        optimizer_d.step()
+
+        # Training the generator
+        g.train()
+        optimizer_g.zero_grad()
+
+        # Passing fake images (without discarding the G gradient)
+        # Evaluating the error against the real image labels 
+        z_predictions = d(g(z))
+        g_loss = loss_function(z_predictions, x_labels)
+
+        # Updating the weights
+        g_loss.backward()
+        optimizer_g.step()
+
+    if (epoch + 1) % 10 == 0:
+        g.eval()
+        with torch.no_grad():
+            g_images = g(fixed_noise)
+            g_images =(g_images + 1.0) / 2.0
+            file_path = f"./images/gan_epoch_progress_{epoch+1}.png"
+            utils.save_image(g_images, file_path, nrow=3)
+            print(f"Image progress grid saved in: {file_path}")
+
